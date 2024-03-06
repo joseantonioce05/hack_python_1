814a7c08a87419887f80c29c57a18ebac2dba082
joseantonioce <joseantonioce.06@gmail.com>
2024-03-05 21:42:05 -0400

    Hack terminado

--- hack_7.py
+++ hack_7.py
@@ -4,5 +4,8 @@ loop: while [] output => [5,4,3,2,1,0]
 
 def fn_hack_7():
     result = []
-    #...
+    counter = 5
+    while counter > -1:
+        result.append(counter)
+        counter -= 1
     return result  
--- hack_9.py
+++ hack_9.py
@@ -4,5 +4,13 @@ loop: while [1,2,3] ouput => [1,'@',2,'@',3,'@']
 
 def fn_hack_9():
     result = [1,2,3]
-    #...
+    counter = 1
+    while counter <= 3:
+        index = result.index(counter)
+        index += 1
+        result.insert(index, "@")
+        counter += 1
     return result  
+
+f = fn_hack_9()
+print(f)
